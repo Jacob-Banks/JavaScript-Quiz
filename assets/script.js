69fb30b7dc346cd5fdd8507d2733a7807338d1cc
Jacob Banks <kobybanks@gmail.com>
2021-06-09 11:26:56 -0400

    reorganized script functions, changed testscores sort, changed testscores array

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-let timer = 99;
-let score = "";
 let start = document.getElementById("btn");
 let quiz = document.getElementById("quiz");
-let testScores = [];
 let high = document.getElementById("high");
-let index = 0;
+let index = 0; //which question
+let timer = 99;
+let score = "";
+let testScores = [];
 
 const questions = [
   "aaaaaaaaaaa",
@@ -45,6 +45,23 @@ let answers = [
     ["tuuuuuuuuuuuuuuuuuuuuuwwwwwwww", "right"],
   ],
 ];
+
+function timerScore() {
+  //time countdown
+  score = setInterval(function () {
+    // when time runs out stop the timer go to highsscores
+    if (timer <= 0) {
+      clearInterval(score);
+      document.getElementById("timer").innerHTML = "Failed";
+      highScores();
+    } else {
+      // if time is left show time
+      document.getElementById("timer").innerHTML = "Score " + timer;
+    }
+    timer -= 1; // deduct time
+  }, 1000); // every second
+}
+
 function startQuiz(myArray) {
   //randomize answer order
   for (var i = 0; i < myArray.length; i++) {
@@ -57,11 +74,11 @@ function startQuiz(myArray) {
       myArray[i][j] = tempk;
     }
   }
-  //hide unsed elements
+  //hide unused elements
   document.getElementById("btn").style.display = "none";
   document.getElementById("sub").style.display = "none";
 
-  //start the timer
+  //start the timer/score
   timerScore();
   //display the question
   document.getElementById("headline").innerHTML = questions[index];
@@ -69,6 +86,48 @@ function startQuiz(myArray) {
   createOl();
   //increase counter
   index++;
+}
+
+function createOl() {
+  //create ol to display answers
+  const x = document.createElement("OL");
+  x.setAttribute("id", "Ol");
+  quiz.appendChild(x);
+
+  // fill the list from ansewrs array
+  for (i = 0; i < answers[0].length; i++) {
+    const li = document.createElement("li");
+    li.setAttribute("id", `li${i}`);
+    li.setAttribute("class", "btn");
+    let text = document.createTextNode(`${i + 1}. ${answers[0][i][0]}`);
+    li.appendChild(text);
+    document.getElementById("Ol").appendChild(li);
+  }
+  // create the right/wrong notifier container
+  const para = document.createElement("p");
+  const body = document.body;
+  para.setAttribute("id", `result`);
+  body.appendChild(para);
+
+  // add event listener idetifiers for li
+  let list1 = document.querySelector("#li0");
+  let list2 = document.querySelector("#li1");
+  let list3 = document.querySelector("#li2");
+  let list4 = document.querySelector("#li3");
+
+  // identifier on all list items for border removal
+  const border = document.querySelectorAll(".btn");
+  //remove border and previous result on mouse over
+  border.forEach((el) =>
+    el.addEventListener("mouseover", (event) => {
+      resetBorder();
+    })
+  );
+  // add listeners for which li was clicked, launch neext question pass which li
+  list1.addEventListener("click", () => nextquestion(0));
+  list2.addEventListener("click", () => nextquestion(1));
+  list3.addEventListener("click", () => nextquestion(2));
+  list4.addEventListener("click", () => nextquestion(3));
 }
 
 function nextquestion(li) {
@@ -88,136 +147,128 @@ function nextquestion(li) {
   //increse counter
   index++;
 }
-
-function timerScore() {
-  //time countdown
-  score = setInterval(function () {
-    if (timer <= 0) {
-      clearInterval(score);
-      document.getElementById("timer").innerHTML = "Failed";
-    } else {
-      document.getElementById("timer").innerHTML = "score " + timer;
-    }
-    timer -= 1;
-  }, 1000);
-}
-
-function exit() {
-  // change headline
-  document.getElementById("headline").innerHTML = "All done";
-  //hide unsued elements
-  document.getElementById("Ol").style.display = "none";
-  // display user score
-  document.getElementById("sub").style.display = "block";
-  document.getElementById("sub").innerHTML = `Your score is ${timer}`;
-
-  // stop timer
-  clearInterval(score);
-
-  //removes timer
-  document.getElementById("timer").style.display = "none";
-
-  //add submit initials and score form
-  createForm();
-}
-
-// sets border adds wheter previous q was right or wrong subtracts score for wrong entry
+// sets border adds whether previous q was right or wrong subtracts score for wrong entry
 function setBorder(li) {
   quiz.style.borderBottom = "solid grey 1px";
   document.getElementById("result").style.display = "block";
   if (answers[index - 1][li][1] === "right") {
-    document.getElementById("result").innerHTML = "correct";
+    document.getElementById("result").innerHTML = "Correct!";
   } else {
-    document.getElementById("result").innerHTML = "wrong";
+    document.getElementById("result").innerHTML = "Wrong ;(";
     timer = timer - 10;
   }
 }
 
-//removes border
+//removes border and previous result on mouse over li
 function resetBorder() {
   quiz.style.border = "none";
   document.getElementById("result").style.display = "none";
 }
 
-function createOl() {
-  //create ol to display answers
-  const x = document.createElement("OL");
-  x.setAttribute("id", "Ol");
+function exit() {
+  // change headline
+  document.getElementById("headline").innerHTML = "All done!";
+  // display user score
+  document.getElementById("sub").style.display = "block";
+  document.getElementById("sub").innerHTML = `Your score is ${timer}`;
+  //add submit initials and score form
+  createForm();
+
+  //hide unsued elements
+  document.getElementById("Ol").style.display = "none";
+  document.getElementById("timer").style.display = "none";
+  // stop timer
+  clearInterval(score);
+}
+
+function createForm() {
+  //create the  submit your name + score form
+  const x = document.createElement("form");
+  x.setAttribute("id", "form");
   quiz.appendChild(x);
 
-  // fill the list from ansewrs array
-  for (i = 0; i < answers[0].length; i++) {
-    const li = document.createElement("li");
-    li.setAttribute("id", `li${i}`);
-    li.setAttribute("class", "btn");
-    let text = document.createTextNode(`${i + 1}. ${answers[0][i][0]}`);
-    li.appendChild(text);
-    document.getElementById("Ol").appendChild(li);
-  }
-  // create the right/wrong notifyer container
-  const para = document.createElement("p");
-  const body = document.body;
-  para.setAttribute("id", `result`);
-  body.appendChild(para);
+  // add input field
+  const y = document.createElement("input");
+  y.setAttribute("type", "text");
+  y.setAttribute("id", "formInput");
+  y.setAttribute("placeholder", "asdf");
+  document.getElementById("form").appendChild(y);
 
-  // add event listener idetifiers for li
-  let list1 = document.querySelector("#li0");
-  let list2 = document.querySelector("#li1");
-  let list3 = document.querySelector("#li2");
-  let list4 = document.querySelector("#li3");
-  // identifier on all list items for border removal
-  const border = document.querySelectorAll(".btn");
-  //remove border on mouse over
-  border.forEach((el) =>
-    el.addEventListener("mouseover", (event) => {
-      resetBorder();
-    })
-  );
-  // add listeners for which li was clicked launch neext question
-  list1.addEventListener("click", () => nextquestion(0));
-  list2.addEventListener("click", () => nextquestion(1));
-  list3.addEventListener("click", () => nextquestion(2));
-  list4.addEventListener("click", () => nextquestion(3));
+  //add a submit button
+  const button = document.createElement("button");
+  button.setAttribute("id", "formButton");
+  button.setAttribute("class", "btn");
+  button.setAttribute("type", "button");
+  document.getElementById("form").appendChild(button);
+
+  let text = document.createTextNode("submit");
+  document.getElementById("formButton").appendChild(text);
+
+  // make the button launch add score which fills highscores and adds this entry
+  let addInfo = document.querySelector("#formButton");
+  addInfo.addEventListener("click", () => addScore());
+
+  //stop  enter key submit
+  let form = document.getElementById("form");
+  function handleForm(event) {
+    event.preventDefault();
+  }
+  form.addEventListener("submit", handleForm);
+} //end of form
+
+function addScore() {
+  //get previous scores
+  testScores = JSON.parse(localStorage.getItem("testScores"));
+  if (testScores == null) testScores = [];
+
+  //get  and set current quiz score
+  const user = document.getElementById("formInput").value;
+  const score = timer;
+  const scoreValue = [score, user];
+
+  // place score in high score array
+  testScores.push(scoreValue);
+  //sort scores
+  testScores.sort(function (a, b) {
+    return b[0] - a[0];
+  });
+  // limit length
+  if (testScores.length > 10) {
+    testScores.pop();
+  }
+  // send scores to storage
+  localStorage.setItem("testScores", JSON.stringify(testScores));
+
+  //launch high score page
+  highScores();
 }
 
 function highScores() {
-  //get rid of submit score form
-  if (index > 5) {
-    document.getElementById("form").style.display = "none";
-    document.getElementById("btn").style.display = "none";
-  }
-  // get rid of answer ol
-  if (index > 0) {
-    document.getElementById("Ol").style.display = "none";
-    resetBorder();
-  }
-  //get rid of subtitle
-  document.getElementById("sub").style.display = "none";
-
   //set the new headline
   document.getElementById("headline").innerHTML = "High Scores";
 
   // get the saved high scores
-  var testScores = JSON.parse(localStorage.getItem("testScores"));
+  testScores = JSON.parse(localStorage.getItem("testScores"));
   if (testScores == null) testScores = [];
 
   // create a list that displays high scores
   const x = document.createElement("OL");
   x.setAttribute("id", "Ol2");
   quiz.appendChild(x);
-  var aScore = "";
-  for (var i = 0; i < testScores.length; i++) {
-    aScore += "<li>" + testScores[i] + "</li>";
+
+  //create and add li(s)aka scores
+  let aScore = "";
+  for (let i = 0; i < testScores.length; i++) {
+    aScore += "<li>" + testScores[i][0] + " " + testScores[i][1] + "</li>";
   }
   document.getElementById("Ol2").innerHTML = aScore;
 
-  // create start again button
+  // create go back button
   const button = document.createElement("button");
   button.setAttribute("type", "submit");
   button.setAttribute("id", "highButton1");
   button.setAttribute("class", "btn");
   document.getElementById("Ol2").appendChild(button);
-
   let text = document.createTextNode("Go Back");
   document.getElementById("highButton1").appendChild(text);
 
@@ -233,64 +284,35 @@ function highScores() {
   buttonB.setAttribute("class", "btn");
   buttonB.style.marginLeft = "10px";
   document.getElementById("Ol2").appendChild(buttonB);
-
   let textB = document.createTextNode("Clear High Scores");
   document.getElementById("highButton2").appendChild(textB);
 
-  // make clear scores
+  // make  button clear scores
   document
     .getElementById("highButton2")
     .addEventListener("click", () => clearHigh());
-}
 
-function createForm() {
-  //creat the form
-  const x = document.createElement("form");
-  x.setAttribute("id", "form");
-  quiz.appendChild(x);
+  //get rid of submit score form
+  if (index > answers.length) {
+    document.getElementById("form").style.display = "none";
+  }
+  // get rid of answer ol
+  if (index > 0) {
+    document.getElementById("Ol").style.display = "none";
+    resetBorder();
+  }
+  //get rid of subtitle
+  document.getElementById("sub").style.display = "none";
+  document.getElementById("btn").style.display = "none";
+} //end of highscores
 
-  // add input field
-  const y = document.createElement("input");
-  y.setAttribute("type", "text");
-  y.setAttribute("id", "formInput");
-  y.setAttribute("placeholder", "asdf");
-  document.getElementById("form").appendChild(y);
-
-  //add a submit button
-  const button = document.createElement("button");
-
-  button.setAttribute("id", "formButton");
-  button.setAttribute("class", "btn");
-  button.setAttribute("type", "button");
-  document.getElementById("form").appendChild(button);
-
-  let text = document.createTextNode("submit");
-  document.getElementById("formButton").appendChild(text);
-
-  // make button to launch add entry which fills highscores and adds this entry
-  let wtf = document.querySelector("#formButton");
-  wtf.addEventListener("click", () => addEntry());
-}
-
-function addEntry() {
-  // get previous scores
-  var testScores = JSON.parse(localStorage.getItem("testScores"));
-  if (testScores == null) testScores = [];
-  //set the current score and user
-  var value = document.getElementById("formInput").value;
-  scoreValue = ` ${timer}` + "____" + `${value} `;
-  // Save  back to local storage
-  localStorage.setItem("scoreValue", JSON.stringify(scoreValue));
-  // place score in high score array
-  testScores.push(scoreValue);
-  localStorage.setItem("testScores", JSON.stringify(testScores));
-  //launch high score page
-  highScores();
-}
+//clears local storage
 function clearHigh() {
   window.localStorage.clear();
   highScores();
 }
 
+// start call to action
 start.addEventListener("click", () => startQuiz(answers));
+// view high scores call to action
 high.addEventListener("click", () => highScores());
